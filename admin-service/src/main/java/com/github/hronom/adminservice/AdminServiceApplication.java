357b24259384dcf8296e71d2d049924499c5ed65
Eugene <hronom@gmail.com>
2018-02-25 17:23:28 +0200

    Added admin service

---
+++ admin-service/src/main/java/com/github/hronom/adminservice/AdminServiceApplication.java
@@ -0,0 +1,14 @@
+package com.github.hronom.adminservice;
+
+import de.codecentric.boot.admin.config.EnableAdminServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@EnableAdminServer
+@SpringBootApplication
+public class AdminServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AdminServiceApplication.class, args);
+    }
+}
